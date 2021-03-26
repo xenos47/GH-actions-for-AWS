aabe6de5dd19b4b03d509f0149988e9f80ec0fd5
Igor Kropochev <xenos47@gmail.com>
2021-03-26 21:15:28 +0700

    Тестируем запуск ALB

--- launch_inst.sh
+++ launch_inst.sh
@@ -1,24 +1,27 @@
 #! /bin/bash
 
-instances=$(aws ec2 run-instances --launch-template LaunchTemplateId=$1,Version=$2 --count 2 --query "Instances"[].InstanceId --output text)
+#instances=$(aws ec2 run-instances --launch-template LaunchTemplateId=$1,Version=$2 --count 2 --query "Instances"[].InstanceId --output text)
 
-for inst_id in $instances; do
+#for inst_id in $instances; do
+#
+#	while true; 
+#	do
+#		inst_st="$(aws ec2 describe-instance-status --instance-id $inst_id --include-all-instances --query "InstanceStatuses"[].InstanceState.Code --output text)"
+#			if [ "$inst_st" = "16" ] ; then
+#				
+#				break # or add more commands to finilize the process
+#			fi
+#
+#			echo "Status of instance with id: ${inst_id} is: ${inst_st}"
+#			sleep 1; 
+#        done
+#
+#	echo "Instance with id: ${inst_id} is running!!!"
+#
+#done
 
-	while true; 
-	do
-		inst_st="$(aws ec2 describe-instance-status --instance-id $inst_id --include-all-instances --query "InstanceStatuses"[].InstanceState.Code --output text)"
-			if [ "$inst_st" = "16" ] ; then
-				
-				break # or add more commands to finilize the process
-			fi
+lb_arn=$(aws elbv2 create-load-balancer --name $3 --subnets $4 $5 --security-groups $6)
 
-			echo "Status of instance with id: ${inst_id} is: ${inst_st}"
-			sleep 1; 
-        done
-
-	echo "Instance with id: ${inst_id} is running!!!"
-
-done
-
+echo "LoadBalancerArn: ${lb_arn}"
 
 #inst_st=$(($inst_st + 1)) Получение цифровых значений из строки в bash
