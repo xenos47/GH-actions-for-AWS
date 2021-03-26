6643909490cdd7c823d997b399a3e062d04d26b1
Igor Kropochev <xenos47@gmail.com>
2021-03-26 20:40:55 +0700

    Добавлен тестовый workflow, bash-скрипт отредактирован под запуск в Actions

--- launch_inst.sh
+++ launch_inst.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-instances=$(sudo docker run --rm -v ~/.aws:/root/.aws amazon/aws-cli ec2 run-instances --launch-template LaunchTemplateId=lt-04b30e2dc9209f94b,Version=1 --count 2 --query "Instances"[].InstanceId --output text)
+instances=$(aws ec2 run-instances --launch-template LaunchTemplateId=$1,Version=$2 --count 2 --query "Instances"[].InstanceId --output text)
 
 for inst_id in $instances; do
 
 	while true; 
 	do
-		inst_st="$(sudo docker run --rm -v ~/.aws:/root/.aws amazon/aws-cli ec2 describe-instance-status --instance-id $inst_id --include-all-instances --query "InstanceStatuses"[].InstanceState.Code --output text)"
+		inst_st="$(aws ec2 describe-instance-status --instance-id $inst_id --include-all-instances --query "InstanceStatuses"[].InstanceState.Code --output text)"
 			if [ "$inst_st" = "16" ] ; then
 				
 				break # or add more commands to finilize the process
