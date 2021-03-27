b5cbf7be1f67a53e65a1da39de5d7cb007fd0bb4
xenos47 <81171976+xenos47@users.noreply.github.com>
2021-03-27 12:54:24 +0700

    Update instances.sh

--- instances.sh
+++ instances.sh
@@ -18,15 +18,14 @@ for inst_id in $instances; do
         done
 
 	echo "Instance with id: ${inst_id} is running!!!"
-	echo "Id=${inst_id}"
 	id_instances+=("Id=${inst_id}")
 	
 	        
 done
-#
-#for inst_id1 in $id_instances; do
-#	echo "The new format: ${inst_id1}"
-#done
+
+for inst_id1 in $id_instances; do
+	echo "The new format: ${inst_id1}"
+done
 
 
 #inst_st=$(($inst_st + 1)) Получение цифровых значений из строки в bash
