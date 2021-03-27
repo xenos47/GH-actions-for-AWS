9d35df0bd9883a288e23fc130ff66a81b2b0f8e8
xenos47 <81171976+xenos47@users.noreply.github.com>
2021-03-27 22:02:53 +0700

    Create testing-bash.sh

---
+++ testing-bash.sh
@@ -0,0 +1,13 @@
+#!bash
+
+var=11
+slots=3
+result=$((var / slots))
+k=$((var % slots ))
+for ((i=0; i<k; i++)); do
+  variableLengthArray[i]=$(( result + 1 ))
+done
+for ((i=k; i < slots; i++)); do
+ variableLengthArray[i]=$result
+done
+echo "$result"
