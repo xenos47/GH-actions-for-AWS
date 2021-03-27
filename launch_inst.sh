b2b71c68b84695e8c44dfd0f0e5a393216c5a9f8
Igor Kropochev <xenos47@gmail.com>
2021-03-27 11:18:20 +0700

    Необходимо добавить Id- в

--- launch_inst.sh
+++ launch_inst.sh
@@ -23,10 +23,11 @@ done
 lb_arn=$(aws elbv2 create-load-balancer --name $3 --subnets $4 $5 --security-groups $6 --query "LoadBalancers"[].LoadBalancerArn --output text)
 vpc_id=$(aws elbv2 describe-load-balancers --load-balancer-arns $lb_arn --query "LoadBalancers"[].VpcId --output text)
 tgrp_arn=$(aws elbv2 create-target-group --name $7 --protocol HTTP --port 80 --vpc-id $vpc_id --query "TargetGroups"[].TargetGroupArn --output text)
-aws elbv2 register-targets --target-group-arn $tgrp_arn --targets $instances
-aws elbv2 create-listener --load-balancer-arn $lb_arn --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$tgrp_arn
+#aws elbv2 register-targets --target-group-arn $tgrp_arn --targets $instances
+#aws elbv2 create-listener --load-balancer-arn $lb_arn --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$tgrp_arn
 
 
 
 #echo "LoadBalancerArn: ${lb_arn} VpcId: ${vpc_id} TargetGroupArn: ${tgrp_arn}"
 
+#Добавить Id= в $instances!!!
