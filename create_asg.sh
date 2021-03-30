3c6e148a5ba1ba5dc6a67656825bf69d5dac9a72
xenos47 <81171976+xenos47@users.noreply.github.com>
2021-03-30 22:15:56 +0700

    Update create_asg.sh

--- create_asg.sh
+++ create_asg.sh
@@ -3,14 +3,13 @@
 # Скрипт создания ASG (EC2 Auto Scaling) по шаблону и балансировщика нагрузки 
 #  
 # Требуемые входные данные:
-# --------------------------------------------------------- 
+# -----------------------------------------------------------------------------
 # ID и версия шаблона (предварительно созданного) $1 $2
 # Имя ALB и TG для создаваемой инфраструктуры $3 $4
 # ID Security Group для балансировщика (ПОКА предварительно созданной) $5
 #
-# Скрипт для настройки серверов и деплоя приложений script.txt
-#
-# Пример: ./launch_inst.sh 4 lt-04b30e2dc9209f94b 1 test-ALB test-target-group sg-0be89dd414f0479d5
+# Минимальное, максимальное и желаемое при старте количество инстансов $6 $7 $8
+# ------------------------------------------------------------------------------
 
 # Считываем имеющиеся подсети в массив $subnets
 read -a subnets <<< $(aws ec2 describe-subnets --query "Subnets"[].SubnetId --output text)
@@ -31,4 +30,4 @@ aws elbv2 create-listener --load-balancer-arn $lb_arn --protocol HTTP --port 80 
 # Создаём ASG
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name my-asg --launch-template LaunchTemplateId=$1,Version=$2 \
     --vpc-zone-identifier "$subnets_str" --target-group-arns $tgrp_arn \
-    --min-size 1 --max-size 3 --desired-capacity 2  
+    --min-size $6 --max-size $7 --desired-capacity $8  
