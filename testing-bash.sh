86e07cb1d8863b803c57c0b3b42fd6d8204ec7ae
xenos47 <81171976+xenos47@users.noreply.github.com>
2021-03-31 12:07:57 +0700

    Update testing-bash.sh

--- testing-bash.sh
+++ testing-bash.sh
@@ -8,4 +8,4 @@
 read -a subnets <<< $(aws ec2 describe-subnets --query "Subnets"[].SubnetId --output text)
 
 # Создаём Load Balancer
-aws elbv2 create-load-balancer --name "$1_ALB" --subnets ${subnets[*]} --security-groups $2 > /dev/null
+aws elbv2 create-load-balancer --name "$1-ALB" --subnets ${subnets[*]} --security-groups $2 > /dev/null
